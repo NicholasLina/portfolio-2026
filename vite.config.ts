685eb5be48e3a9dac4d787127bf5f24cec40c8a1
NicholasLina <nicktlina@gmail.com>
2026-01-15 15:35:52 -0500

    vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./src/test/setup.ts'],
+        css: true,
+        globals: true
+    },
     build: {
         outDir: 'dist',
         sourcemap: false,
